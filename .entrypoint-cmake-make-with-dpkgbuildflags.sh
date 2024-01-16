d4c35257c2db3c1e9ead004a3e197bd06e804926
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-01-16 19:04:50 +0100

    Added a Dockerfile for testing
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

---
+++ .entrypoint-cmake-make-with-dpkgbuildflags.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+. /opt/ros/rolling/setup.sh
+
+mkdir src/ros-drake-vendor/build
+cd src/ros-drake-vendor/build
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_PREFIX_PATH=/opt/ros/rolling -DAMENT_PREFIX_PATH=/opt/ros/rolling -DCMAKE_INSTALL_PREFIX=/opt/ros/rolling -DCMAKE_BUILD_TYPE=None
+make -j1 || exec "$@"
+exec "$@"
